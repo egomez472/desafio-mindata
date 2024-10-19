634dc1d1310e3b43b2471cb902ca0c3218c48727
Eric <eric.gomez.unq@gmail.com>
2024-10-18 22:39:47 -0300

    Modelo de hero creado. Comentarios y modulos importados eliminados

---
+++ src/app/core/models/hero.model.ts
@@ -0,0 +1,9 @@
+export class HeroModel {
+  constructor(
+    public id: number,
+    public name: string,
+    public alias: string,
+    public powers: string[],
+    public team: string
+  ) {}
+}
--- src/app/core/services/heroes.service.ts
+++ src/app/core/services/heroes.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
-import { initializeApp } from 'firebase/app';
-import { getDownloadURL, getStorage, ref, uploadBytes, uploadString } from 'firebase/storage';
-import { environment } from '../../environments/environment';
+import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage';
 
 @Injectable({
   providedIn: 'root'
@@ -10,37 +8,14 @@ export class HeroesService {
 
   private storage = getStorage();
 
-  constructor() {}
-
-  // public async uploadHeroImage(name: string, imageBase64: any) {
-  //   const storageRef = ref(this.storage, name);
-  //   let response = await uploadString(storageRef, imageBase64, 'base64');
-  //   console.log(response);
-  // }
-
   public async uploadHeroImage(name: string, file: File) {
-    const storageRef = ref(this.storage, name); // referencia al archivo en storage
-    // const base64String = imageBase64.split(',')[1];
+    const storageRef = ref(this.storage, name);
 
     const metadata = {
-      contentType: file.type // Establecer el tipo MIME correcto
+      contentType: file.type
     };
 
     let response = await uploadBytes(storageRef, file, metadata).then(() => getDownloadURL(storageRef));
     return response
-
-    // return uploadString(storageRef, base64String, 'base64')
-    //   .then(() => {
-    //     // Obtener el URL de descarga de la imagen
-    //     return getDownloadURL(storageRef);
-    //   })
-    //   .then((downloadURL) => {
-    //     console.log('File available at', downloadURL);
-    //     return downloadURL; // Devuelve la URL de la imagen subida
-    //   })
-    //   .catch((error) => {
-    //     console.error('Error uploading image:', error);
-    //     throw error; // Manejo de errores
-    //   });
   }
 }
