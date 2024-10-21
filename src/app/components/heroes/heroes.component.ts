0808703eec60182e4032aa14f47a4b04659ad865
Eric <eric.gomez.unq@gmail.com>
2024-10-21 10:07:55 -0300

    Organizo codigo en heroes.component

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -16,38 +16,27 @@ import { NgxPaginationModule } from 'ngx-pagination';
 })
 export class HeroesComponent implements OnInit {
 
-  @Input() id!: string;
-  @Input() maxSize!: number;
-  @Output() pageChange!: EventEmitter<number>;
-  @Output() pageBoundsCorrection!: EventEmitter<number>;
-
   private readonly heroesSvc = inject(HeroesService);
   private readonly router = inject(Router);
 
   heroesList = signal<Hero[]>([]);
   p = signal(1);
-  itemsPerPage = 5;
-
-  // Función para hacer el trackBy por id
-  trackById(index: number, hero: any): number {
-    return hero.id;
-  }
-
-  paginatedHeroes = computed(() => {
-    const startIndex = (this.p() - 1) * this.itemsPerPage;
-    const endIndex = startIndex + this.itemsPerPage;
-    return this.heroesList().slice(startIndex, endIndex);
-  });
-
-  onPageChange(newPage: number) {
-    this.p.set(newPage);
-  }
 
   ngOnInit(): void {
     this.heroesSvc.getHeroes().subscribe((data) => {
       this.heroesList.set(data);// Actualizo la signal con los héroes recibidos
     });
   }
+
+  //#region ngx-pagination functions
+  trackById(index: number, hero: any): number {
+    return hero.id;
+  }
+
+  onPageChange(newPage: number) {
+    this.p.set(newPage);
+  }
+  //#endregion ngx-pagination functions
 
   navigate(url:string) {
     this.router.navigate([url])
